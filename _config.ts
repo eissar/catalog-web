9b6e3ddc6095b005c43640d6d091ee053407f18e
Eshaan Issar <eshaanissar@outlook.com>
2026-04-02 21:10:27 -0400

    Add Lume PageFind plugin

--- _config.ts
+++ _config.ts
@@ -1,6 +1,14 @@
 import lume from "lume/mod.ts";
+import pagefind from "lume/plugins/pagefind.ts";
 
 // Configure Lume to use the 'catalog' directory as the source content directory
 const site = lume({ src: "./catalog" });
+
+// Enable PageFind full-text search plugin
+site.use(pagefind({
+  // default options can be overridden here
+  // For example, customize UI container id:
+  // ui: { containerId: "search" },
+}));
 
 export default site;
